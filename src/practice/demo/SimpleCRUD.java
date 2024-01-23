af0e6decfb6eafd16df57df20dca8847dc2017fd
Ajay Dubey <ajdubey.qa@gmail.com>
2024-01-23 23:59:41 +0530

    Added Simple crud program

---
+++ src/practice/demo/SimpleCRUD.java
@@ -0,0 +1,103 @@
+package practice.demo;
+
+import java.util.Scanner;
+
+public class SimpleCRUD {
+
+	int row;
+	final int col;
+	String[][] users;
+	int rowCount = 0;
+
+	public SimpleCRUD() {
+		row = 10;
+		col = 2;
+		users = new String[row][col];
+	}
+
+	public <users> String[][] addUser(String userName, String Email) {
+		
+		if(Email.endsWith(".com") || Email.endsWith(".in")) {
+			if (rowCount <= row) {
+				users[rowCount][0] = userName;
+				users[rowCount][1] = Email;
+				rowCount++;
+			}
+			else {
+				System.out.println("No Space");
+
+			}
+		}else 
+		{
+			System.out.println("Invalid EmailID..Suported type .com or .in");
+		}
+
+		 
+		return users;
+	}
+
+	public void getAllUserEmail() {
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				if (users[i][j] != null) {
+				
+					if(j==0) 
+					{
+						System.out.println("userName :" + users[i][j]);
+					}else if(j==1) 
+					{
+						System.out.println("email :" + users[i][j]);
+						System.out.println("**********************");
+					}
+					
+				}
+				
+			}
+		
+		}
+	}
+
+	public void addMultiuser(int no) {
+		
+		Scanner sc = new Scanner(System.in);
+		while (no > 1) {
+			System.out.println("Enter your Email && userName");
+			String userName = sc.next();
+			String email = sc.next();
+			addUser(userName, email);
+			no--;
+
+		}
+		sc.close();
+	
+
+	}
+	
+	
+	public void getSingleUser(String userName) 
+	{
+		for(int i =0; i< row; i++) 
+		{
+			for(int j =0; j<col; j++) 
+			{
+				if(users[i][0]!=null && users[i][0].toString().equals(userName))
+				{
+					System.out.println("user Details are => ");
+					System.out.println(users[i][0]);
+					System.out.println(users[i][1]);
+					break;
+					
+				}else if((users[i][0]!=null || users[i][0]==null) && i ==row-1 && j==0) 
+				{
+					System.out.println("No user found With given Name");
+					
+				}
+				
+			}
+				
+			
+		}
+		
+	}
+
+}
---
+++ src/practice/demo/UsersRC.java
@@ -0,0 +1,17 @@
+package practice.demo;
+
+public class UsersRC {
+
+	public static void main(String[] args) {
+		
+		
+		SimpleCRUD sc = new SimpleCRUD();
+		sc.addUser("akshay", "abcd@yopmail.hotmail");
+		sc.addUser("Akk", "acd@yopmail.com");
+		sc.addMultiuser(2);
+		sc.getAllUserEmail();
+		sc.getSingleUser("akshay");
+
+	}
+
+}
